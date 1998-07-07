597b149b6bb7311555a6a84450e3bd143437d6d2
bll <bll@localhost>
1998-07-06 18:07:30 -0700

    Initial revision

---
+++ C/getopt.c
@@ -0,0 +1,91 @@
+/*
+ *
+ * $Id$
+ *
+ * getopt - get option letter from argv
+ *
+ * from Cnews by Henry Spencer
+ *
+ */
+
+#ifndef lint
+static char    getopt_c_rcsid [] =
+"$Id$";
+static char    getopt_c_source [] =
+"$Source$";
+#endif
+
+#include <stdio.h>
+#include <string.h>
+
+char    *optarg;    /* Global argument pointer. */
+int optind = 0; /* Global argv index. */
+
+static char *scan = NULL;   /* Private scan pointer. */
+
+int
+getopt(argc, argv, optstring)
+int argc;
+char *argv[];
+char *optstring;
+{
+    char c;
+    char *place;
+
+
+    optarg = NULL;
+
+    if (scan == NULL || *scan == '\0')
+    {
+        if (optind == 0)
+        {
+            optind++;
+        }
+
+        if (optind >= argc || argv[optind][0] != '-' ||
+                argv[optind][1] == '\0')
+        {
+            return(EOF);
+        }
+
+        if (strcmp(argv[optind], "--")==0)
+        {
+            optind++;
+            return(EOF);
+        }
+
+        scan = argv[optind]+1;
+        optind++;
+    }
+
+    c = *scan++;
+    place = strchr(optstring, c);
+
+    if (place == NULL || c == ':')
+    {
+        fprintf(stderr, "%s: unknown option -%c\n", argv[0], c);
+        return('?');
+    }
+
+    place++;
+    if (*place == ':')
+    {
+        if (*scan != '\0')
+        {
+            optarg = scan;
+            scan = NULL;
+        }
+        else if (optind < argc)
+        {
+            optarg = argv[optind];
+            optind++;
+        }
+        else
+        {
+            fprintf(stderr, "%s: -%c argument missing\n", argv[0], c);
+            return('?');
+        }
+    }
+
+    return(c);
+}
