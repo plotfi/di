d56339ab5bac71b9291e9e23af2541c23132aba1
bll <bll@localhost>
2010-01-31 23:59:23 -0800

    don't need exclusion

--- tests.d/ditot.sh
+++ tests.d/ditot.sh
@@ -7,7 +7,7 @@ FORMATS="b B u c f v i U F"
 grc=0
 for format in $FORMATS; do
   echo "Checking format: $format"
-  didata=`../di -n -d1 -f $format -t -x lofs`
+  didata=`../di -n -d1 -f $format -t`
   summtot=`(echo "0 ";echo $didata | sed 's/  */ + /g'; echo " - p") | dc`
   if [ $summtot -ne 0 ]; then
     echo "format: $format failed"
