51bdf406048003a6f5181edb05e5996430761380
bll <bll@localhost>
2010-02-20 00:03:22 -0800

    compare cache also

--- C/tests.d/compshpl.sh
+++ C/tests.d/compshpl.sh
@@ -1,17 +1,24 @@
 #!/bin/sh
 
-echo ${EN} "di compare mkconfig.sh mkconfig.pl${EC}" >&3
+echo ${EN} "compare mkconfig.sh mkconfig.pl${EC}" >&3
 
 cd ..
+grc=0
 make distclean
-rm -f config.h.sh config.h.pl
+rm -f config.h.sh config.h.pl cache.pl cache.sh
 ./Build -mksh config.h
 mv config.h config.h.sh
+cat mkconfig.cache | sort > cache.sh
 rm -f mkconfig.cache
 ./Build -mkpl config.h
 mv config.h config.h.pl
+cat mkconfig.cache | sort > cache.pl
 diff -w config.h.sh config.h.pl
 rc=$?
-rm -f config.h.sh config.h.pl
+if [ $rc -ne 0 ]; then grc=$rc; fi
+diff -w cache.sh cache.pl
+rc=$?
+if [ $rc -ne 0 ]; then grc=$rc; fi
+rm -f config.h.sh config.h.pl cache.pl cache.sh
 
-exit $rc
+exit $grc
