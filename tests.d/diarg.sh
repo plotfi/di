0dc417b2ea25ff420936e5c7d24f38459241d750
bll <bll@localhost>
2010-02-20 12:35:15 -0800

    fix cygwin test

--- tests.d/diarg.sh
+++ tests.d/diarg.sh
@@ -2,14 +2,17 @@
 
 echo ${EN} "di w/argument${EC}" >&3
 
+# most all unix
 ../di -n -f M / 2>/dev/null | egrep '^/[ ]*$' > /dev/null 2>&1
 rc=$?
 if [ $rc -ne 0 ]; then
-  ../di -n -f M C: | egrep '^C:\\[ ]*$' > /dev/null 2>&1
+  # cygwin
+  ../di -n -f M / 2>/dev/null | egrep '^C:\\[ ]*$' > /dev/null 2>&1
   rc=$?
   if [ $rc -ne 0 ]; then
+    # other machines w/odd setup
     ../di -n -f M /boot 2>/dev/null | egrep '^/boot[ ]*$' > /dev/null 2>&1
     rc=$?
-  fi
+  FI
 fi
 exit $rc
