7c035fa52169d1bb8bdce9e12c72d0570596665f
bll <bll@localhost>
2010-03-02 01:48:46 -0800

    update for new dir structure.

--- mktar.sh
+++ mktar.sh
@@ -2,12 +2,28 @@
 
 echo "Enter version: \c"
 read ver
+
 rm -rf di-${ver} > /dev/null 2>&1
 mkdir di-${ver}
+mkdir di-${ver}/mkconfig
 chmod 755 di-${ver}
-cp -r $* di-${ver}
+chmod 755 di-${ver}/mkconfig
+
+for f in $@; do
+  case $f in 
+    mkconfig*)
+      cp -r $f di-${ver}/mkconfig
+      ;;
+    *)
+      cp -r $f di-${ver}
+      ;;
+  esac
+done
 chmod -R a+r di-${ver}/*
+
 tar cf - ./di-${ver} |
 gzip -9 > di-${ver}.tar.gz
+
 rm -rf di-${ver} > /dev/null 2>&1
+
 exit 0
