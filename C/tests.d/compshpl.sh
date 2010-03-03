51b01fac4b4c134efc81b3d9d1ca46763b94137d
bll <bll@localhost>
2010-03-03 00:09:03 -0800

    change target

--- C/tests.d/compshpl.sh
+++ C/tests.d/compshpl.sh
@@ -9,12 +9,12 @@ cd ..
 grc=0
 make distclean
 rm -f config.h.sh config.h.pl cache.pl cache.sh vars.pl vars.sh
-make -e config-sh
+make -e config.h
 mv config.h config.h.sh
 cat mkconfig.cache | sort > cache.sh
 cat mkconfig.vars > vars.sh
 rm -f mkconfig.cache
-make -e config-perl
+make -e MKCONFIG_TYPE=perl config.h
 mv config.h config.h.pl
 cat mkconfig.cache | sort > cache.pl
 cat mkconfig.vars > vars.pl
