5c17abccd97cd7fc8a947e754de5b061006435e5
bll <bll@localhost>
2010-03-08 09:16:09 -0800

    chg egrep to grep

--- tests.d/dinls.sh
+++ tests.d/dinls.sh
@@ -16,7 +16,7 @@ fi
 grc=1
 for l in "de" "de_DE" "de_DE.utf-8" "de_DE.UTF-8" \
     "de_DE.ISO8859-1" "de_DE.ISO8859-15"; do
-  LC_ALL="${l}" ../test_di/bin/di -A | egrep Benutzt >/dev/null 2>&1
+  LC_ALL="${l}" ../test_di/bin/di -A | grep Benutzt >/dev/null 2>&1
   rc=$?
   if [ $rc -eq 0 ]; then
     grc=0
