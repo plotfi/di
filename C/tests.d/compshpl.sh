43da0a00921042f2dcaa60cef60a6be47f653071
bll <bll@localhost>
2010-03-13 22:38:10 -0800

    fix so no leftover vars file for next test

--- C/tests.d/compshpl.sh
+++ C/tests.d/compshpl.sh
@@ -7,29 +7,35 @@ echo ${EN} "compare mkconfig.sh mkconfig.pl${EC}" >&3
 
 cd $RUNTOPDIR
 grc=0
+
 make distclean
 rm -f config.h.sh config.h.pl cache.pl cache.sh vars.pl vars.sh
+
 make -e config.h
 mv config.h config.h.sh
 cat mkconfig.cache | grep -v '^di_env' | sort > cache.sh
-cat mkconfig_c.vars > vars.sh
+mv mkconfig_c.vars vars.sh
+
 rm -f mkconfig.cache
+
 make -e MKCONFIG_TYPE=perl config.h
 mv config.h config.h.pl
 cat mkconfig.cache | sort > cache.pl
-cat mkconfig_c.vars > vars.pl
+mv mkconfig_c.vars vars.pl
+
 echo "##== diff config.h.sh config.h.pl"
 diff -w config.h.sh config.h.pl
 rc=$?
 if [ $rc -ne 0 ]; then grc=$rc; fi
+
 echo "##== diff cache.sh cache.pl"
 diff -w cache.sh cache.pl
 rc=$?
 if [ $rc -ne 0 ]; then grc=$rc; fi
+
 echo "##== diff vars.sh vars.pl"
 diff -w vars.sh vars.pl
 rc=$?
 if [ $rc -ne 0 ]; then grc=$rc; fi
-rm -f config.h.sh config.h.pl cache.pl cache.sh vars.pl vars.sh
 
 exit $grc
