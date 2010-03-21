d2e55bee2ed69cc04c8a5cc0fa30e1583ea6cd36
bll <bll@localhost>
2010-03-20 17:27:25 -0700

    test variety of shells

--- tests.d/buildsh.sh
+++ tests.d/buildsh.sh
@@ -6,9 +6,40 @@
 echo ${EN} "build w/mkconfig.sh${EC}" >&3
 
 cd $RUNTOPDIR
-make distclean
-instdir="`pwd`/test_di"
-make -e prefix=${instdir} all-sh
-rc=$?
+grc=0
+dotest () {
+  echo ${EN} "${_MKCONFIG_SHELL} ${EC}" >&3
+  echo "   testing with ${_MKCONFIG_SHELL} "
+  make distclean
+  instdir="`pwd`/test_di"
+  time make -e prefix=${instdir} all-sh
+  rc=$?
+  if [ $rc != 0 ]; then grc=$rc; fi
+}
 
-exit $rc
+echo ${EN} " ${EC}" >&3
+if [ -x /bin/bash ]; then
+  export _MKCONFIG_SHELL=bash
+  dotest
+fi
+if [ -x /bin/ksh ]; then
+  tshell=`/usr/bin/ksh -c ". $mypath/shellfuncs.sh;getshelltype"`
+  case $tshell in
+    pdksh)
+      ;;
+    *)
+      _MKCONFIG_SHELL=ksh
+      dotest
+      ;;
+  esac
+fi
+if [ -x /bin/ash ]; then
+  _MKCONFIG_SHELL=ash
+  dotest
+fi
+if [ -x /bin/dash ]; then
+  _MKCONFIG_SHELL=dash
+  dotest
+fi
+
+exit $grc
