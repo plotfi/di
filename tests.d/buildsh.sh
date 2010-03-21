f71093ac64b9ceb914cb97aa50295acbb1484fc9
bll <bll@localhost>
2010-03-20 18:26:53 -0700

    check for symlinks ignore symlinks to /etc/alternatives check for sh

--- tests.d/buildsh.sh
+++ tests.d/buildsh.sh
@@ -18,33 +18,46 @@ dotest () {
 }
 
 echo ${EN} " ${EC}" >&3
-if [ -x /bin/bash ]; then
-  _MKCONFIG_SHELL=bash
-  export _MKCONFIG_SHELL
-  dotest
-fi
-if [ -x /usr/bin/ksh ]; then
-  cmd="/usr/bin/ksh -c \". $mypath/shellfuncs.sh;getshelltype;echo \\\$shell\""
-  tshell=`eval $cmd`
-  case $tshell in
-    pdksh)
-      ;;
-    *)
-      _MKCONFIG_SHELL=ksh
+for s in /bin/sh /bin/bash /bin/posh /bin/ash /bin/dash; do
+  if [ -x $s ]; then
+    ls -l $s | grep -- '->' > /dev/null 2>&1
+    rc1=$?
+    ls -l $s | grep '/etc/alternatives' > /dev/null 2>&1
+    rc2=$?
+    if [ $rc1 -ne 0 -o $rc2 -eq 0 ]; then
+      _MKCONFIG_SHELL=`echo $s | sed 's,.*/,,'`
       export _MKCONFIG_SHELL
       dotest
-      ;;
-  esac
-fi
-if [ -x /bin/ash ]; then
-  _MKCONFIG_SHELL=ash
-  export _MKCONFIG_SHELL
-  dotest
-fi
-if [ -x /bin/dash ]; then
-  _MKCONFIG_SHELL=dash
-  export _MKCONFIG_SHELL
-  dotest
-fi
+    fi
+  fi
+done
+for s in /usr/bin/ksh /bin/ksh; do 
+  if [ $s = "/bin/ksh" ]; then
+    ls -l /bin | grep -- '->' > /dev/null 2>&1
+    if [ $? -eq 0 ]; then
+      continue
+    fi
+  fi
+
+  if [ -x $s ]; then
+    ls -l $s | grep -- '->' > /dev/null 2>&1
+    rc1=$?
+    ls -l $s | grep '/etc/alternatives' > /dev/null 2>&1
+    rc2=$?
+    if [ $rc1 -ne 0 -o $rc2 -eq 0 ]; then
+      cmd="$s -c \". $mypath/shellfuncs.sh;getshelltype;echo \\\$shell\""
+      tshell=`eval $cmd`
+      case $tshell in
+        pdksh)
+          ;;
+        *)
+          _MKCONFIG_SHELL=ksh
+          export _MKCONFIG_SHELL
+          dotest
+          ;;
+      esac
+    fi
+  fi
+done
 
 exit $grc
