43cf28325c57276ac65051f7869f9f0440b8b95f
bll <bll@localhost>
2010-03-24 23:12:52 -0700

    chg paths; use new func for shell list

--- tests.d/buildsh.sh
+++ tests.d/buildsh.sh
@@ -6,6 +6,12 @@
 echo ${EN} "build w/mkconfig.sh${EC}" >&3
 
 dotest () {
+  sh=$1
+
+  _MKCONFIG_SHELL=${sh}
+  export _MKCONFIG_SHELL
+  cmd="$sh -c \". $MKCONFIG_DIR/shellfuncs.sh;getshelltype;echo \\\$shell\""
+  shell=`eval $cmd`
   echo ${EN} "${shell} ${EC}" >&3
   echo "   testing with ${_MKCONFIG_SHELL} "
   make distclean
@@ -15,54 +21,14 @@ dotest () {
   if [ $rc != 0 ]; then grc=$rc; fi
 }
 
-cd $RUNTOPDIR
 grc=0
 echo ${EN} " ${EC}" >&3
 
-plist=""
-ls -ld /bin | grep -- '->' > /dev/null 2>&1
-if [ $? -ne 0 ]; then
-  plist=/bin
-fi
-plist="${plist} /usr/bin /usr/local/bin"
-
-for p in $plist; do
-  for s in sh bash posh ash dash mksh; do
-    if [ -x $p/$s ]; then
-      ls -l $p/$s | grep -- '->' > /dev/null 2>&1
-      rc1=$?
-      ls -l $p/$s | grep '/etc/alternatives' > /dev/null 2>&1
-      rc2=$?
-      if [ $rc1 -ne 0 -o $rc2 -eq 0 ]; then
-        _MKCONFIG_SHELL=${p}/${s}
-        export _MKCONFIG_SHELL
-        shell=$s
-        dotest
-      fi
-    fi
-  done
-  for s in ksh; do
-    if [ -x $p/$s ]; then
-      ls -l $p/$s | grep -- '->' > /dev/null 2>&1
-      rc1=$?
-      ls -l $p/$s | grep '/etc/alternatives' > /dev/null 2>&1
-      rc2=$?
-      if [ $rc1 -ne 0 -o $rc2 -eq 0 ]; then
-        cmd="$p/$s -c \". $mypath/shellfuncs.sh;getshelltype;echo \\\$shell\""
-        tshell=`eval $cmd`
-        case $tshell in
-          pdksh)
-            ;;
-          *)
-            _MKCONFIG_SHELL=${p}/${s}
-            export _MKCONFIG_SHELL
-            shell=$s
-            dotest
-            ;;
-        esac
-      fi
-    fi
-  done
+. $MKCONFIG_DIR/shellfuncs.sh
+cd $RUNTOPDIR
+getlistofshells
+for s in $shelllist; do
+  dotest $s
 done
 
 exit $grc
