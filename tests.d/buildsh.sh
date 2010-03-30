e8d91e646c76f662d7b08628997e17d4b9b194f2
bll <bll@localhost>
2010-03-29 21:56:25 -0700

    update env vars

--- tests.d/buildsh.sh
+++ tests.d/buildsh.sh
@@ -12,7 +12,7 @@ dotest () {
   export _MKCONFIG_SHELL
   unset _shell
   unset shell
-  cmd="$sh -c \". $MKCONFIG_DIR/shellfuncs.sh;getshelltype;echo \\\$shell\""
+  cmd="$sh -c \". $_MKCONFIG_DIR/shellfuncs.sh;getshelltype;echo \\\$shell\""
   shell=`eval $cmd`
   if [ "$shell" = "sh" ]; then
     shell=`echo ${_MKCONFIG_SHELL} | sed 's,.*/,,'`
@@ -29,7 +29,7 @@ dotest () {
 grc=0
 echo ${EN} " ${EC}" >&3
 
-. $MKCONFIG_DIR/shellfuncs.sh
+. $_MKCONFIG_DIR/shellfuncs.sh
 cd $RUNTOPDIR
 getlistofshells
 for s in $shelllist; do
