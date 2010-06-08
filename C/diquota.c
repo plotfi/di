8276ee34c97727a89eabfcaffc844314114ac16c
bll <bll@localhost>
2010-06-08 09:46:00 -0700

    various fixes for tru64/aix; only use 'used' if there's a limit set. constants

--- C/diquota.c
+++ C/diquota.c
@@ -59,12 +59,12 @@
 #endif
 
 #if _hdr_rpc_rpc && _hdr_rpcsvc_rquota
-static bool_t xdr_quota_get _((XDR *, getquota_args *));
-static bool_t xdr_quota_rslt _((XDR *, getquota_rslt *));
+static bool_t xdr_quota_get _((XDR *, struct getquota_args *));
+static bool_t xdr_quota_rslt _((XDR *, struct getquota_rslt *));
 static void diquota_nfs _((diQuota_t *));
 #endif
 #if _enable_quotas
-static void di_process_quotas _((diQuota_t *, int, int, char *));
+static void di_process_quotas _((char *, diQuota_t *, int, int, char *));
 #endif
 
 extern int debug;
@@ -76,10 +76,14 @@ extern int debug;
 #ifdef BLOCK_SIZE           /* linux */
 # define DI_QUOT_BLOCK_SIZE BLOCK_SIZE
 #else
-# ifdef DEV_BSIZE           /* tru64, et. al. */
-#  define DI_QUOT_BLOCK_SIZE DEV_BSIZE
+# ifdef DQBSIZE             /* AIX */
+#  define DI_QUOT_BLOCK_SIZE DQBSIZE
 # else
-#  define DI_QUOT_BLOCK_SIZE 512
+#  ifdef DEV_BSIZE           /* tru64 */
+#   define DI_QUOT_BLOCK_SIZE DEV_BSIZE
+#  else
+#   define DI_QUOT_BLOCK_SIZE 512
+#  endif
 # endif
 #endif
 
@@ -92,13 +96,6 @@ extern int debug;
 #endif
 #if _mem_dqb_curfiles_dqblk
 # define dqb_curinodes dqb_curfiles
-#endif
-
- /* AIX has the linux compatibility call, */
- /* but it still requires a path rather   */
- /* than the special device name          */
-#if _AIX
-# define special name
 #endif
 
 void
@@ -148,12 +145,13 @@ diquota (diqinfo)
     gcmd = QCMD (Q_XGETQUOTA, GRPQUOTA);
     qiptr = (char *) &xfsqinfo;
     xfsflag = TRUE;
-  } else {
 # endif
+    ;
+  } else {
 # ifdef QCMD
     ucmd = QCMD (Q_GETQUOTA, USRQUOTA);
     gcmd = QCMD (Q_GETQUOTA, GRPQUOTA);
-#endif
+# endif
     qiptr = (char *) &qinfo;
   }
 
@@ -162,8 +160,13 @@ diquota (diqinfo)
         (int) diqinfo->uid, (caddr_t) qiptr);
 # endif
 # if _lib_quotactl && _quotactl_pos == 2
+#  if _AIX  /* AIX has linux compatibility routine, but still need name */
+  rc = quotactl (ucmd, diqinfo->name,
+        (int) diqinfo->uid, (caddr_t) qiptr);
+#  else
   rc = quotactl (ucmd, diqinfo->special,
         (int) diqinfo->uid, (caddr_t) qiptr);
+#  endif
 # endif
 # if _sys_fs_ufs_quota        /* Solaris */
   {
@@ -183,39 +186,47 @@ diquota (diqinfo)
   }
 # endif  /* _sys_fs_ufs_quota */
 
-  di_process_quotas (diqinfo, rc, xfsflag, qiptr);
+  di_process_quotas ("usr", diqinfo, rc, xfsflag, qiptr);
 
+# ifdef GRPQUOTA
   if (rc == 0 || errno != ESRCH) {
 #  if _lib_quotactl && _quotactl_pos == 1
     rc = quotactl (diqinfo->name, gcmd,
           (int) diqinfo->gid, (caddr_t) qiptr);
 #  endif
-#  if _lib_quotactl && _quotactl_pos == 2
+#   if _lib_quotactl && _quotactl_pos == 2
     rc = quotactl (gcmd, diqinfo->special,
              (int) diqinfo->gid, (caddr_t) qiptr);
 #  endif
 
-    di_process_quotas (diqinfo, rc, xfsflag, qiptr);
+    di_process_quotas ("grp", diqinfo, rc, xfsflag, qiptr);
   }
+# endif /* ifdef GRPQUOTA */
 #endif /* _enable_quotas */
 }
 
 #if _hdr_rpc_rpc && _hdr_rpcsvc_rquota
 
+#ifdef RQ_PATHLEN
+# define DI_RQ_PATHLEN  RQ_PATHLEN
+#else
+# define DI_RQ_PATHLEN  1024
+#endif
+
 static bool_t
 # if _proto_stdc
-xdr_quota_get (XDR *xp, getquota_args *args)
+xdr_quota_get (XDR *xp, struct getquota_args *args)
 # else
 xdr_quota_get (xp, args)
-  XDR           *xp;
-  getquota_args *args;
+  XDR                   *xp;
+  struct getquota_args  *args;
 # endif
 {
   if (debug > 5) {
     printf ("quota: xdr_quota_get\n");
   }
 
-  if (! xdr_string (xp, &args->gqa_pathp, RQ_PATHLEN)) {
+  if (! xdr_string (xp, &args->gqa_pathp, DI_RQ_PATHLEN)) {
     return 0;
   }
   if (! xdr_int (xp, &args->gqa_uid)) {
@@ -226,15 +237,15 @@ xdr_quota_get (xp, args)
 
 static bool_t
 # if _proto_stdc
-xdr_quota_rslt (XDR *xp, getquota_rslt *rslt)
+xdr_quota_rslt (XDR *xp, struct getquota_rslt *rslt)
 # else
 xdr_quota_rslt (xp, rslt)
-  XDR           *xp;
-  getquota_rslt *rslt;
+  XDR                   *xp;
+  struct getquota_rslt  *rslt;
 # endif
 {
-  int       quotastat;
-  rquota    *rptr;
+  int           quotastat;
+  struct rquota *rptr;
 
   if (debug > 5) {
     printf ("quota: xdr_quota_rslt\n");
@@ -243,8 +254,16 @@ xdr_quota_rslt (xp, rslt)
   if (! xdr_int (xp, &quotastat)) {
     return 0;
   }
+# if _mem_gqr_status_getquota_rslt
+  rslt->gqr_status = quotastat;
+# else
   rslt->status = quotastat;
+# endif
+# if _mem_gqr_rquota_getquota_rslt
+  rptr = &rslt->gqr_rquota;
+# else
   rptr = &rslt->getquota_rslt_u.gqr_rquota;
+# endif
 
   if (! xdr_bool (xp, &rptr->rq_active)) {
     return 0;
@@ -281,26 +300,27 @@ diquota_nfs (diqinfo)
   diQuota_t     *diqinfo;
 # endif
 {
-    CLIENT          *rqclnt;
-    enum clnt_stat  clnt_stat;
-    struct timeval  timeout = {2, 0};
-    char            host [DI_SPEC_NAME_LEN];
-    char            *ptr;
-    char            *path;
-    getquota_args   args;
-    getquota_rslt   result;
-    rquota          *rptr;
-    _fs_size_t      tsize;
-    _fs_size_t      quotBlockSize = { DI_QUOT_BLOCK_SIZE };
+    CLIENT                  *rqclnt;
+    enum clnt_stat          clnt_stat;
+    struct timeval          timeout = {2, 0};
+    char                    host [DI_SPEC_NAME_LEN];
+    char                    *ptr;
+    char                    *path;
+    struct getquota_args    args;
+    struct getquota_rslt    result;
+    struct rquota           *rptr;
+    int                     quotastat;
+    _fs_size_t              tsize;
+    _fs_size_t              quotBlockSize = { 1024 };
 
 
     if (debug > 5) {
-      printf ("quota: diquota_nfs\n");
+      printf ("quota: diquota_nfs block size: %d\n", quotBlockSize);
     }
 
     strcpy (host, diqinfo->special);
     path = host;
-    ptr = index (host, ':');
+    ptr = strchr (host, ':');
     if (ptr != (char *) NULL) {
       *ptr = '\0';
       path = ptr + 1;
@@ -308,8 +328,8 @@ diquota_nfs (diqinfo)
     if (debug > 2) {
       printf ("quota: nfs: host: %s path: %s\n", host, path);
     }
-	args.gqa_pathp = path;
-	args.gqa_uid = (int) diqinfo->uid;
+    args.gqa_pathp = path;
+    args.gqa_uid = (int) diqinfo->uid;
 
     rqclnt = clnt_create (host, RQUOTAPROG, RQUOTAVERS, "udp");
     if (rqclnt == (CLIENT *) NULL) {
@@ -333,18 +353,27 @@ diquota_nfs (diqinfo)
       return;
     }
 
-    if (result.status == 1) {
+# if _mem_gqr_status_getquota_rslt
+    quotastat = result.gqr_status;
+# else
+    quotastat = result.status;
+# endif
+    if (quotastat == 1) {
       if (debug > 2) {
         printf ("quota: nfs: status 1\n");
       }
+# if _mem_gqr_rquota_getquota_rslt
+      rptr = &result.gqr_rquota;
+# else
       rptr = &result.getquota_rslt_u.gqr_rquota;
+# endif
 
-      /* diqinfo->blockSize is something too large to use */
+      /* diqinfo->blockSize is sometimes too large to use */
       /* reset it.                                        */
-      diqinfo->blockSize = 1024;
+      if (diqinfo->blockSize > 1024) {
+        diqinfo->blockSize = 1024;
+      }
 
-      diqinfo->used = rptr->rq_curblocks *
-            quotBlockSize / diqinfo->blockSize;
       diqinfo->limit = rptr->rq_bhardlimit *
             quotBlockSize / diqinfo->blockSize;
       tsize = rptr->rq_bsoftlimit *
@@ -352,28 +381,38 @@ diquota_nfs (diqinfo)
       if (tsize != 0 && tsize < diqinfo->limit) {
         diqinfo->limit = tsize;
       }
-      diqinfo->iused = rptr->rq_curfiles;
+      if (diqinfo->limit != 0) {
+        diqinfo->used = rptr->rq_curblocks *
+              quotBlockSize / diqinfo->blockSize;
+      }
+
       diqinfo->ilimit = rptr->rq_fhardlimit;
-      if (rptr->rq_fsoftlimit < diqinfo->ilimit) {
-        diqinfo->ilimit = rptr->rq_fsoftlimit;
+      tsize = rptr->rq_fsoftlimit;
+      if (tsize != 0 && tsize < diqinfo->ilimit) {
+        diqinfo->ilimit = tsize;
+      }
+      if (diqinfo->ilimit != 0) {
+        diqinfo->iused = rptr->rq_curfiles;
       }
     }
 
-	if (rqclnt) {
+    if (rqclnt) {
       if (rqclnt->cl_auth) {
         auth_destroy (rqclnt->cl_auth);
       }
       clnt_destroy (rqclnt);
-	}
+    }
 }
 #endif  /* have rpc headers */
 
 #if _enable_quotas
 static void
 # if _proto_stdc
-di_process_quotas (diQuota_t *diqinfo, int rc, int xfsflag, char *cqinfo)
+di_process_quotas (char *tag, diQuota_t *diqinfo,
+                  int rc, int xfsflag, char *cqinfo)
 # else
-di_process_quotas (diqinfo, rc, xfsflag, cqinfo)
+di_process_quotas (tag, diqinfo, rc, xfsflag, cqinfo)
+  char          *tag;
   diQuota_t     *diqinfo;
   int           rc;
   int           xfsflag;
@@ -382,6 +421,7 @@ di_process_quotas (diqinfo, rc, xfsflag, cqinfo)
 {
   _fs_size_t        quotBlockSize = { DI_QUOT_BLOCK_SIZE };
   _fs_size_t        tsize;
+  _fs_size_t        tlimit;
   struct dqblk      *qinfo;
 # if _typ_fs_disk_quota_t
   fs_disk_quota_t   *xfsqinfo;
@@ -396,6 +436,52 @@ di_process_quotas (diqinfo, rc, xfsflag, cqinfo)
   }
 
   if (rc == 0) {
+    tlimit = 0;
+    if (xfsflag) {
+# if _typ_fs_disk_quota_t
+      tsize = xfsqinfo->d_blk_hardlimit;
+# endif
+      ;
+    } else {
+      tsize = qinfo->dqb_bhardlimit;
+    }
+    if (debug > 2) {
+      printf ("quota: %s %s orig hard: %lld\n", tag, diqinfo->name, tsize);
+    }
+    tsize = tsize * quotBlockSize / diqinfo->blockSize;
+    if (tsize != 0 && (tsize < diqinfo->limit || diqinfo->limit == 0)) {
+      diqinfo->limit = tsize;
+      tlimit = tsize;
+    }
+
+    if (xfsflag) {
+# if _typ_fs_disk_quota_t
+      tsize = xfsqinfo->d_blk_softlimit;
+# endif
+      ;
+    } else {
+      tsize = qinfo->dqb_bsoftlimit;
+    }
+    if (debug > 2) {
+      printf ("quota: %s %s orig soft: %lld\n", tag, diqinfo->name, tsize);
+    }
+    tsize = tsize * quotBlockSize / diqinfo->blockSize;
+    if (tsize != 0 && (tsize < diqinfo->limit || diqinfo->limit == 0)) {
+      if (debug > 2) {
+        printf ("quota: using soft: %lld\n", tsize);
+      }
+      diqinfo->limit = tsize;
+      tlimit = tsize;
+    }
+
+      /* any quota set? */
+    if (tlimit == 0) {
+      if (debug > 2) {
+        printf ("quota: %s %s no quota\n", tag, diqinfo->name);
+      }
+      return;
+    }
+
 # if _mem_dqb_curspace_dqblk
     tsize = qinfo->dqb_curspace / diqinfo->blockSize;
     if (tsize > diqinfo->used || diqinfo->used == 0) {
@@ -417,44 +503,49 @@ di_process_quotas (diqinfo, rc, xfsflag, cqinfo)
     }
 # endif
 
-    if (xfsflag) {
-# if _typ_fs_disk_quota_t
-      tsize = xfsqinfo->d_blk_hardlimit;
-# endif
-      ;
-    } else {
-      tsize = qinfo->dqb_bhardlimit;
-    }
     if (debug > 2) {
-      printf ("quota: orig hard: %lld\n", tsize);
-    }
-    tsize = tsize * quotBlockSize / diqinfo->blockSize;
-    if (tsize != 0 && (tsize < diqinfo->limit || diqinfo->limit == 0)) {
-      diqinfo->limit = tsize;
-    }
-
-    if (xfsflag) {
-# if _typ_fs_disk_quota_t
-      tsize = xfsqinfo->d_blk_softlimit;
-# endif
-      ;
-    } else {
-      tsize = qinfo->dqb_bsoftlimit;
-    }
-    if (debug > 2) {
-      printf ("quota: orig soft: %lld\n", tsize);
-    }
-    tsize = tsize * quotBlockSize / diqinfo->blockSize;
-    if (tsize != 0 && (tsize < diqinfo->limit || diqinfo->limit == 0)) {
-      if (debug > 2) {
-        printf ("quota: using soft: %lld\n", tsize);
-      }
-      diqinfo->limit = tsize;
-    }
-
-    if (debug > 2) {
-      printf ("quota: %s used: %lld limit: %lld\n", diqinfo->name,
+      printf ("quota: %s %s used: %lld limit: %lld\n", tag, diqinfo->name,
           diqinfo->used, diqinfo->limit);
+    }
+
+    if (xfsflag) {
+# if _typ_fs_disk_quota_t
+      tsize = xfsqinfo->d_ino_hardlimit;
+# endif
+      ;
+    } else {
+      tsize = qinfo->dqb_ihardlimit;
+    }
+    if (tsize != 0 && (tsize < diqinfo->ilimit || diqinfo->ilimit == 0)) {
+      diqinfo->ilimit = tsize;
+      tlimit = tsize;
+    }
+    if (debug > 2) {
+      printf ("quota: %s %s i hard: %lld\n", tag, diqinfo->name, tsize);
+    }
+
+    if (xfsflag) {
+# if _typ_fs_disk_quota_t
+      tsize = xfsqinfo->d_ino_softlimit;
+# endif
+      ;
+    } else {
+      tsize = qinfo->dqb_isoftlimit;
+    }
+    if (tsize != 0 && (tsize < diqinfo->ilimit || diqinfo->ilimit == 0)) {
+      diqinfo->ilimit = tsize;
+      tlimit = tsize;
+    }
+    if (debug > 2) {
+      printf ("quota: %s %s i soft: %lld\n", tag, diqinfo->name, tsize);
+    }
+
+      /* any quota set? */
+    if (tlimit == 0) {
+      if (debug > 2) {
+        printf ("quota: %s %s no inode quota\n", tag, diqinfo->name);
+      }
+      return;
     }
 
     if (xfsflag) {
@@ -468,33 +559,12 @@ di_process_quotas (diqinfo, rc, xfsflag, cqinfo)
     if (tsize > diqinfo->iused || diqinfo->iused == 0) {
       diqinfo->iused = tsize;
     }
-
-    if (xfsflag) {
-# if _typ_fs_disk_quota_t
-      tsize = xfsqinfo->d_ino_hardlimit;
-# endif
-      ;
-    } else {
-      tsize = qinfo->dqb_ihardlimit;
-    }
-    if (tsize != 0 && tsize < (diqinfo->ilimit || diqinfo->ilimit == 0)) {
-      diqinfo->ilimit = tsize;
-    }
-
-    if (xfsflag) {
-# if _typ_fs_disk_quota_t
-      tsize = xfsqinfo->d_ino_softlimit;
-# endif
-      ;
-    } else {
-      tsize = qinfo->dqb_isoftlimit;
-    }
-    if (tsize != 0 && tsize < (diqinfo->ilimit || diqinfo->ilimit == 0)) {
-      diqinfo->ilimit = tsize;
+    if (debug > 2) {
+      printf ("quota: %s %s i used: %lld\n", tag, diqinfo->name, tsize);
     }
   } else {
     if (debug > 2) {
-      printf ("quota: %s errno %d\n", diqinfo->name, errno);
+      printf ("quota: %s %s errno %d\n", tag, diqinfo->name, errno);
     }
   }
 }
