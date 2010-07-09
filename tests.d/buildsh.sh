92485d60b49a254542681373e68dd677ac2bd607
bll <bll@localhost>
2010-07-09 07:43:24 -0700

    shell loop moved to runtests.sh

--- tests.d/buildsh.sh
+++ tests.d/buildsh.sh
@@ -11,42 +11,18 @@ fi
 . $_MKCONFIG_DIR/shellfuncs.sh
 testshcapability
 
-count=1
-
-dotest () {
-  sh=$1
-
-  _MKCONFIG_SHELL=${sh}
-  export _MKCONFIG_SHELL
-  unset _shell
-  unset shell
-  cmd="$sh -c \". $_MKCONFIG_DIR/shellfuncs.sh;getshelltype;echo \\\$shell\""
-  shell=`eval $cmd`
-  if [ "$shell" = "sh" ]; then
-    shell=`echo ${_MKCONFIG_SHELL} | sed 's,.*/,,'`
-  fi
-  echo ${EN} "${shell} ${EC}" >&5
-  echo "##"
-  echo "##   testing with ${_MKCONFIG_SHELL} "
-  echo "##"
-  make distclean
-  instdir="`pwd`/test_di"
-  make -e prefix=${instdir} all-sh
-  rc=$?
-  if [ $rc != 0 ]; then grc=$rc; fi
-  mkdir -p $_MKCONFIG_TSTRUNTMPDIR/${count}_${shell}
-  # leave these laying around for use by install.sh and rpmbuild.sh test
-  cp mkconfig.log mkconfig.cache mkconfig*.vars di.env reqlibs.txt \
-      $_MKCONFIG_TSTRUNTMPDIR/${count}_${shell}
-  domath count "$count + 1"
-}
-
 grc=0
-echo ${EN} " ${EC}" >&5
 
 cd $_MKCONFIG_RUNTOPDIR
-for s in $shelllist; do
-  dotest $s
-done
+
+instdir="`pwd`/test_di"
+make distclean
+make -e prefix=${instdir} all-sh
+rc=$?
+if [ $rc != 0 ]; then grc=$rc; fi
+mkdir -p $_MKCONFIG_TSTRUNTMPDIR/buildsh${stag}
+# leave these laying around for use by install.sh and rpmbuild.sh test
+cp mkconfig.log mkconfig.cache mkconfig*.vars di.env reqlibs.txt \
+    $_MKCONFIG_TSTRUNTMPDIR/buildsh${stag}
 
 exit $grc
