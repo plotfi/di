5adf99734a3c7eabb0ad22453a347b9f4cf0e39a
bll <bll@localhost>
2010-07-29 18:22:41 -0700

    check the output for extra stuff.... not yet complete.

--- tests.d/buildsh.sh
+++ tests.d/buildsh.sh
@@ -17,12 +17,36 @@ cd $_MKCONFIG_RUNTOPDIR
 
 instdir="`pwd`/test_di"
 make realclean
-make -e prefix=${instdir} all-sh
+make -e prefix=${instdir} all-sh > make.log 2>&1
 rc=$?
 if [ $rc != 0 ]; then grc=$rc; fi
+
+if [ $grc -eq 0 ]; then
+  cat make.log |
+    grep -v '^load\-unit:' |
+    grep -v '^output\-file:' |
+    grep -v '^option\-file:' |
+    grep -v ' \.\.\. ' |
+    grep -vi mkconfig |
+    grep -v 'Leaving directory' |
+    grep -v 'Entering directory' |
+    grep -v '^cc \-[co]' |
+    grep -v digetentries.o |
+    grep -v 'Using libs:' |
+    grep -v 'di\.env' |
+    grep -v '^CC=' > make_extra.log
+  extra=`wc -l make_extra.log`
+  if [ $extra -ne 0 ]; then
+    echo "## extra output"
+    grc=1
+  fi
+fi
+
 mkdir -p $_MKCONFIG_TSTRUNTMPDIR/buildsh${stag}
 # leave these laying around for use by install.sh and rpmbuild.sh test
 cp mkconfig.log mkconfig.cache mkconfig*.vars di.env mkconfig.reqlibs \
+    make.log make_extra.log \
     $_MKCONFIG_TSTRUNTMPDIR/buildsh${stag}
+rm -f make.log make_extra.log >/dev/null 2>&1
 
 exit $grc
