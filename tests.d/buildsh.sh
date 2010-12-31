ac6df34c62542d74a03307e9f162f517a75cada2
bll <bll@localhost>
2010-12-31 14:31:20 -0800

    warning fixes

--- tests.d/buildsh.sh
+++ tests.d/buildsh.sh
@@ -24,23 +24,50 @@ rc=$?
 if [ $rc != 0 ]; then grc=$rc; fi
 
 if [ $grc -eq 0 ]; then
-  cat make.log |
-    grep -v '^load\-unit:' |
-    grep -v '^output\-file:' |
-    grep -v '^option\-file:' |
-    grep -v ' \.\.\. ' |
-    grep -vi mkconfig |
-    grep -v 'Leaving directory' |
-    grep -v 'Entering directory' |
-    grep -v '^cc \-[co]' |
-    grep -v digetentries.o |
-    grep -v 'Using libs:' |
-    grep -v 'di\.env' |
-    grep -v '^CC=' > make_extra.log
-  extra=`cat make_extra.log | wc -l make_extra.log`
-  if [ $extra -ne 0 ]; then
-    echo "## extra output"
-    grc=1
+  systype=`uname -s`
+  # haiku has too many warnings to deal with
+  if [ "$systype" != "Haiku" ]; then 
+    # openbsd: malloc.h, always misused, in file included, in function
+    # openbsd: unfortunately, some output is on separate lines.
+    # solaris: tokens ignored... (dollar sign)
+    # solaris: Function has no return statement : main
+    # freebsd 4.9: from xxx.c:66: (separate line)
+    # freebsd 4.9: /usr/include....function declaration isn't a prototype
+    # AIX: extra msg from make: Target <target> is up to date
+    # Tru64: complains about long long being a new feature.
+    # Tru64: extra ----^ lines
+    cat make.log |
+      grep -v 'dioptions.dat' |
+      grep -v '^load\-unit:' |
+      grep -v '^output\-file:' |
+      grep -v '^option\-file:' |
+      grep -v ' \.\.\. ' |
+      grep -vi mkconfig |
+      grep -v 'Leaving directory' |
+      grep -v 'Entering directory' |
+      grep -v 'cc \-[co]' |
+      grep -v digetentries.o |
+      grep -v '\*\*\* ' |
+      grep -v 'di\.env' |
+      grep -v '^CC=' |
+      grep -v '#warning' |
+      grep -v 'strcpy.*always misused' |
+      grep -v '^In file included' | 
+      grep -v ': In function' | 
+      grep -v 'tokens ignored at end of directive line' |
+      grep -v 'Function has no return statement : main' |
+      grep -v '^ *from [^ ]*.c:[0-9]*:$' |
+      grep -v '/usr/include.*function declaration.*a prototype' |
+      grep -v '^Target.*is up to date' |
+      grep -v '^------' |
+      grep -v 'is a new feature' |
+      grep -v '^ *typedef.* long long ' |
+      cat > make_extra.log
+    extra=`cat make_extra.log | wc -l`
+    if [ $extra -ne 0 ]; then
+      echo "## extra output"
+      grc=1
+    fi
   fi
 fi
 
