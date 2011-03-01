d3d01ecbead20e3f64538fa1cdaceb885814a8d4
bll <bll@localhost>
2011-02-28 19:27:55 -0800

    remove unneeded stuff

--- D/dispopts.d
+++ D/dispopts.d
@@ -1,6 +1,6 @@
 // written in the D programming language
 
-module di_dispopts;
+module didispopts;
 
 import std.stdio;
 import std.string;
@@ -21,25 +21,7 @@ struct DisplayOpts {
   int               baseDispIdx = idx1024;
   short             width = 8;
   short             inodeWidth = 7;
-  short             maxMountString = 15;
-  short             maxSpecialString = 18;
-  short             maxTypeString = 7;
-  short             maxOptString = 0;
-  short             maxMntTimeString = 0;
   string            dbsstr = "H";
   string            dispBlockLabel;
-  string            blockFormat[];
-  string            blockFormatNR[];   /* no radix */
-  string            blockLabelFormat[];
-  string            inodeFormat[];
-  string            inodeLabelFormat[];
-  string            mountFormat[];
-  string            mTimeFormat[];
-  string            optFormat[];
-  string            specialFormat[];
-  string            typeFormat[];
 }
 
-unittest {
-
-}
